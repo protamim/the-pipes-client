19943f2075d60cf1b8f122c8abbdaba5f97746aa
Tamim <s163.tamim@gmail.com>
2023-11-04 23:10:38 +0600

    layout set

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,10 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "../../pages/shared/Navbar/Navbar";
+import MyFooter from "../../pages/shared/Footer/MyFooter";
 
 
 const MainLayout = () => {
     return (
         <>
             <Navbar />
+            <Outlet />
+            <MyFooter />
         </>
     );
 };

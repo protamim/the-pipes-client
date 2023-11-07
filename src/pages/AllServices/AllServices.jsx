50711174b6ee2bb46e0d445197010384489362db
Tamim <s163.tamim@gmail.com>
2023-11-07 22:23:53 +0600

    created all service page

--- src/pages/AllServices/AllServices.jsx
+++ src/pages/AllServices/AllServices.jsx
@@ -1,20 +1,24 @@
 import allServicesBg from "../../assets/img/all_services-bg.png";
-import {IoMdSearch} from 'react-icons/io'
+import { IoMdSearch } from "react-icons/io";
 import useServices from "../../Hooks/useServices";
+import ServicesCard from "./ServicesCard/ServicesCard";
+import { useState } from "react";
 
 const AllServices = () => {
-  const {data} = useServices();
-  console.log(data);
+  const { data } = useServices();
+  const [loadData, setLoadData] = useState(data)
   return (
     <>
-    {/* Filter section */}
+      {/* Filter section */}
       <section
         className="h-96 flex justify-center items-center"
         style={{ backgroundImage: `url(${allServicesBg})` }}
       >
         <div className="bg-white w-[540px] px-11 py-8 m-8 rounded-md">
           <div className="space-y-5">
-            <h2 className="text-2xl text-center">Choose a service to get started.</h2>
+            <h2 className="text-2xl text-center">
+              Choose a service to get started.
+            </h2>
             <fieldset className="w-full space-y-1 dark:text-gray-100">
               <div className="flex">
                 <input
@@ -25,7 +29,7 @@ const AllServices = () => {
                 />
                 <span className="flex items-center px-3 cursor-pointer sm:text-sm rounded-r-md bg-slate-400 text-white dark:bg-gray-700">
                   <button className="text-lg">
-                    <IoMdSearch/>
+                    <IoMdSearch />
                   </button>
                 </span>
               </div>
@@ -34,9 +38,17 @@ const AllServices = () => {
         </div>
       </section>
       {/* Services section */}
-      <section className="min-h-[40vh] flex justify-center items-center">
-        <div>
-            <h3>You have to show the services/filtered services in this section</h3>
+      <section className="py-10">
+        <div className="container mx-auto px-5">
+          <div className="grid gap-5 grid-cols-4">
+            {loadData?.slice(0, 8).map((prod) => (
+              <ServicesCard key={prod._id} product={prod} />
+            ))}
+          </div>
+          {/* More Button */}
+          <div className="text-center my-8">
+            {data.length > 8 && <button className="bg-indigo-500 px-6 p-1 text-gray-100">More</button>}
+          </div>
         </div>
       </section>
     </>
---
+++ src/pages/AllServices/ServicesCard/ServicesCard.jsx
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types";
+
+const ServicesCard = ({ product }) => {
+  // console.log(Object.keys(product).join(", "));
+  const { _id, serviceName, serviceImage, price, serviceArea, description } =
+    product;
+
+  return (
+    <>
+      <div className="bg-indigo-100 flex gap-3 flex-col justify-between">
+        <div>
+          <img className="h-52 w-full object-cover" src={serviceImage} alt="" />
+        </div>
+        <div className="px-2 space-y-4">
+          <div>
+            <h2 className="text-2xl">{serviceName}</h2>
+          </div>
+          <div>
+            <p>{description.slice(0, 100)}...</p>
+          </div>
+          <div className="flex justify-between items-center">
+            <div>
+              <span className="text-lg">{serviceArea}</span>
+            </div>
+            <div>
+              <p className="text-xl">${price}</p>
+            </div>
+          </div>
+          <div className="text-center">
+            <button className="bg-gray-300 mb-4 mt-5 px-5 py-1">View Details</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+ServicesCard.propTypes = {
+  product: PropTypes.object.isRequired,
+};
+export default ServicesCard;

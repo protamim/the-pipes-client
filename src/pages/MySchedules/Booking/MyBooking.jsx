bb1ef7f881f3e69699dfe064b797ab52d3d47ddb
Tamim <s163.tamim@gmail.com>
2023-12-16 21:41:39 +0600

    Fix an error

--- src/pages/MySchedules/Booking/MyBooking.jsx
+++ src/pages/MySchedules/Booking/MyBooking.jsx
@@ -13,17 +13,17 @@ const MyBooking = () => {
   return (
     <>
       <section>
-        <div className="space-y-5 md:space-y-0">
+        <div className="space-y-5">
           {data?.map((ser) => (
             <div key={ser._id} className="bg-slate-200 grid grid-cols-3 gap-5 items-center justify-end">
               <div>
-                <img src={ser?.serviceImage} alt="" />
+                <img className="h-48" src={ser?.serviceImage} alt="" />
               </div>
               <div>
-                <h3>{ser?.serviceName}</h3>
+                <h3 className="text-xl">{ser?.serviceName}</h3>
               </div>
               <div>
-                <h5>{ser?.price}</h5>
+                <h5>${ser?.price}</h5>
                 <p>{ser?.date}</p>
               </div>
             </div>
